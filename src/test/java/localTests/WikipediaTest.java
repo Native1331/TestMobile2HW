6cb2f389676a3344fda3048bded242b9b3859fee
Natalya.Kadysheva <Native1331>
2022-06-27 07:01:35 +0300

    Add Tests

--- src/test/java/localTests/WikipediaTest.java
+++ src/test/java/localTests/WikipediaTest.java
@@ -1,7 +1,6 @@
 package localTests;
 import com.codeborne.selenide.Condition;
 import io.appium.java_client.AppiumBy;
-import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import static com.codeborne.selenide.Selenide.*;
 import static io.qameta.allure.Allure.step;
@@ -10,6 +9,7 @@ import static io.qameta.allure.Allure.step;
 public class WikipediaTest extends TestBase {
     @Test
     void openApp() {
+        open();
 
         step("Open Wikipedia app", () -> {
             $(AppiumBy.id("org.wikipedia.alpha:id/option_label"))
