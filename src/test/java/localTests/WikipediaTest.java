48187197669ba349522c97d45add7e868db91d16
Natalya.Kadysheva <Native1331>
2022-07-01 10:58:08 +0300

    Add Tests

--- src/test/java/localTests/WikipediaTest.java
+++ src/test/java/localTests/WikipediaTest.java
@@ -25,7 +25,7 @@ public class WikipediaTest extends TestBase {
         step("Choose another language", () -> {
             $(AppiumBy.className("android.widget.LinearLayout")).click();
             $(AppiumBy.xpath("//android.widget.TextView[@text = 'ADD LANGUAGE']")).click();
-            $$(AppiumBy.className("android.widget.LinearLayout")).get(2).click();
+            $$(AppiumBy.className("android.widget.LinearLayout")).get(5).click();
             back();
             $(AppiumBy.xpath("//android.widget.TextView[@text= 'Русский']"))
                     .shouldHave(Condition.text("Русский"));
