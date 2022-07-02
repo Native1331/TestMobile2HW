7e18b749f31410dca99ad2803288b9be13bdb36b
Natalya.Kadysheva <Native1331>
2022-07-02 09:31:39 +0300

    Add Tests

--- src/test/java/localTests/WikipediaTest.java
+++ src/test/java/localTests/WikipediaTest.java
@@ -1,8 +1,10 @@
 package localTests;
+
 import com.codeborne.selenide.Condition;
 import io.appium.java_client.AppiumBy;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+
 import static com.codeborne.selenide.Selenide.*;
 import static io.qameta.allure.Allure.step;
 
@@ -28,8 +30,6 @@ public class WikipediaTest extends TestBase {
         });
         step("Make a syncing", () -> {
             $(AppiumBy.xpath("//android.widget.LinearLayout[3]")).click();
-            //     $(AppiumBy.id("org.wikipedia.alpha:id/secondaryTextView")).click();
-            //     $(AppiumBy.className("android.widget.LinearLayout")).click();
             $$(AppiumBy.className("android.widget.TextView")).get(1)
                     .shouldHave(Condition.text("You can make reading lists from articles you want " +
                             "to read later, even when you’re offline. " +
@@ -44,13 +44,13 @@ public class WikipediaTest extends TestBase {
         step("Open main page", () -> {
             $(AppiumBy.className("android.widget.TextView"))
                     .shouldHave(Condition.text("Search Wikipedia"));
-                    });
+        });
         step(" Search  page with text QA", () -> {
             $$(AppiumBy.className("android.widget.TextView")).get(1).click();
-
-       //             .sendKeys("QA");
-            $(AppiumBy.id("org.wikipedia.alpha:id/page_list_item_title"))
-                    .shouldHave(Condition.text("QUALITY ASSURANCE"));
-        });//
+                   $(AppiumBy.className("android.widget.EditText"))
+                                 .sendKeys("QA");
+                    $(AppiumBy.id("org.wikipedia.alpha:id/page_list_item_title"))
+                            .shouldHave(Condition.text("QUALITY ASSURANCE"));
+        });
     }
 }
