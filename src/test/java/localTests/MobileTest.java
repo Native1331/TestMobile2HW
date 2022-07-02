b605700df559e843f54cbc8a0c616d9e56a7fdd0
Natalya.Kadysheva <Native1331>
2022-07-02 14:05:28 +0300

    Add fix

--- src/test/java/localTests/MobileTest.java
+++ src/test/java/localTests/MobileTest.java
@@ -48,7 +48,7 @@ public class MobileTest extends TestBase {
             $(AppiumBy.className("android.widget.TextView")).click();
                    $(AppiumBy.className("android.widget.EditText"))
                                  .sendKeys("QA");
-                    $$(AppiumBy.className("android.view.ViewGroup")).get(1)
+                    $$(AppiumBy.className("android.view.ViewGroup")).get(0)
                             .shouldHave(Condition.text("QA"));
         });
     }
